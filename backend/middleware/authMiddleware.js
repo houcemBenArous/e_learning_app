70bbedc73753df31a25b1fc160bdd6d9aeb535cf
houcemBenArous <houcem.benarous2001@gmail.com>
2025-12-26 01:05:14 +0100

    create roleMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,19 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "No token" });
+  if (!token)
+    return res.status(401).json({ message: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    // récupérer user complet (id + role)
+    req.user = await User.findById(decoded.id).select("_id role");
+
     next();
-  } catch {
+  } catch (error) {
     res.status(401).json({ message: "Invalid token" });
   }
 };
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -7,9 +7,11 @@ const {
   getCourseById
 } = require("../controllers/courseController");
 
-router.post("/", auth, createCourse);
+router.post("/", auth, role("instructor"), createCourse);
 router.get("/", getCourses);
 router.get("/:id", getCourseById);
-router.post("/:id/enroll", auth, enrollCourse);
+router.put("/:id", auth, role("instructor"), updateCourse);
+router.delete("/:id", auth, role("instructor"), deleteCourse);
+router.post("/:id/enroll", auth, role("student"), enrollCourse);
 
 module.exports = router;
